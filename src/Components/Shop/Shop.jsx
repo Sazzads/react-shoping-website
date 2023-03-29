1157bc99bdc315ba2b9900495f209c8dc1cb7de8
Sazzads <sazzadtk@gmail.com>
2023-03-30 01:52:31 +0600

    final commit

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -13,13 +13,46 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, []);
 
-    useEffect(()=>{
-        const storedCart=getShoppingCart();
-    },[]);
+    useEffect(() => {
+        const storedcart = getShoppingCart();
+        const savedcart = [];
+        //step1: get id of the added product
+        for (const id in storedcart) {
+            //step2: get product from products state by using id
+            const addedProduct = products.find(product => product.id === id)
+            if (addedProduct) {
+                //step 3 add quantity
+                const quantity = storedcart[id];
+                addedProduct.quantity = quantity;
+                //step-4 add addProduct to the saved cart
+                savedcart.push(addedProduct);
+
+            }
+            // console.log('added product', addedProduct)
+
+
+        }
+        //step 5 : set the cart
+        setCart(savedcart);
+    }, [products])
 
 
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        // const newCart = [...cart, product];
+        let newCart = [];
+        // if product doesnt exist in the cart , thn set quantity=1
+        //if exist update quantity by 1
+        const exist = cart.find(pd => pd.id === product.id)
+        if (!exist) {
+            product.quantity = 1
+            newCart = [...cart, product]
+        }
+        else{
+            exist.quantity+1;
+            const remaining=cart.filter(pd=>pd!==product.id);
+            newCart=[...remaining,exist]
+        }
+
         setCart(newCart);
         addToDb(product.id)
     }
